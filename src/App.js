2bc61981d04cf870aa3de4a526fb75133ca0ec14
Vali Masca <vali.masca@Vali-Masca-MBP.local>
2020-07-15 11:47:14 -0700

    Setting up the project manually for React with Webpack

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component} from 'react';
+import { Link, Route } from 'react-router-dom';
+
+import Users from './container/Users';
+import asyncComponent from './hoc/asyncComponent';
+
+const AsyncPizza = asyncComponent(() => {
+    return import('./container/Pizza.js');
+});
+
+class App extends Component {
+    render () {
+        return(
+            <div>
+                <div>
+                    <Link to="/">Users</Link> |
+                    <Link to="/pizza">Pizza</Link>
+                </div>
+                <div>
+                    <Route path="/" exact component={Users} />
+                    <Route path="/pizza" component={AsyncPizza} />
+                </div>
+            </div>
+        )
+    }
+}
